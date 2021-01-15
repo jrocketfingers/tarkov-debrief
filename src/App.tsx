d6f51a7a7d18683202f9a691fcdb8f437eabb5ec
Dusan Kovacevic <dusan.kovacevic@acolin.com>
2021-01-15 21:20:23 +0100

    Resizing works for realzies

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,45 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Stage, Layer, Rect, Image } from 'react-konva';
-import useImage from 'use-image';
-import woodsMapUrl from './woods.png';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import { Stage, Layer, Rect, Image } from "react-konva";
+import useImage from "use-image";
+import woodsMapUrl from "./woods.png";
+import "./App.css";
+
+type Size = { width: number; height: number };
+
+const defaultSize: Size = { width: 300, height: 300 };
 
 function App() {
   const containerRef = useRef<HTMLDivElement>(null);
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
+  const [{ width, height }, setSize] = useState<Size>(defaultSize);
   const [mapImage] = useImage(woodsMapUrl);
 
+  // TODO consider useLayoutEffect
   useEffect(() => {
-    matchCanvasSize();
-    if(containerRef?.current) {
-      containerRef.current.addEventListener('resize', () => {
-        matchCanvasSize();
-      });
+    function resizeListener() {
+      if (containerRef.current) {
+        setSize({
+          width: containerRef.current.offsetWidth,
+          height: containerRef.current.offsetHeight,
+        });
+      } else {
+        setSize(defaultSize);
+      }
     }
-  }, [containerRef]);
 
-  const matchCanvasSize = () => {
-    if(containerRef?.current) {
-      const {offsetWidth: width, offsetHeight: height} = containerRef.current;
-      setWidth(width);
-      setHeight(height);
-    }
-  }
+    resizeListener();
+
+    window.addEventListener("resize", resizeListener);
+    return () => window.removeEventListener("resize", resizeListener);
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        Tarkov Debrief
-      </header>
+      <header className="App-header">Tarkov Debrief</header>
       <div className="Canvas" ref={containerRef}>
         <Stage width={width} height={height}>
           <Layer>
-            <Image
-              image={mapImage}
-              x={20}
-              y={20}
-            />
-            <Rect
-              x={20}
-              y={20}
-              width={250}
-              height={250}
-              fill={"green"}
-            />
+            <Image image={mapImage} x={20} y={20} />
+            <Rect x={20} y={20} width={250} height={250} fill={"green"} />
           </Layer>
         </Stage>
       </div>
