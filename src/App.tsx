d353c1525f0fe4257f1aab49dde67f1a6d6f26fe
Johnny Rocketfingers <developer94@gmail.com>
2021-01-16 20:39:28 +0100

    First attempt at a native penning solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Stage, Layer, Rect, Image } from "react-konva";
+import Konva from "konva";
 import useImage from "use-image";
 import woodsMapUrl from "./woods.png";
 import "./App.css";
+import { KonvaEventObject } from "konva/types/Node";
+import { Vector2d } from "konva/types/types";
 
 type Size = { width: number; height: number };
 
@@ -10,8 +13,59 @@ const defaultSize: Size = { width: 300, height: 300 };
 
 function App() {
   const containerRef = useRef<HTMLDivElement>(null);
+  const freeDrawImageRef = useRef<Konva.Image>(null);
+  const freeDrawLayerRef = useRef<Konva.Layer>(null);
   const [{ width, height }, setSize] = useState<Size>(defaultSize);
   const [mapImage] = useImage(woodsMapUrl);
+
+  const freeDrawCanvas = document.createElement('canvas');
+  const freeDrawCtx = freeDrawCanvas.getContext('2d');
+  freeDrawCtx!.strokeStyle = '#df4b26';
+  freeDrawCtx!.lineJoin = 'round';
+  freeDrawCtx!.lineWidth = 5;
+
+  let isPaint = false;
+  let lastPointerPosition: Vector2d | null;
+
+  const mouseDownHandler = (ev: KonvaEventObject<MouseEvent>) => {
+    const stage = ev.currentTarget as Konva.Stage;
+    lastPointerPosition = stage.getPointerPosition();
+    isPaint = true;
+  }
+
+  const mouseUpHandler = (ev: KonvaEventObject<MouseEvent>) => {
+    isPaint = false;
+  }
+
+  const mouseMoveHandler = (ev: KonvaEventObject<MouseEvent|TouchEvent>) => {
+    if (!isPaint) {
+      return;
+    }
+
+    const stage = ev.currentTarget as Konva.Stage;
+
+    freeDrawCtx!.globalCompositeOperation = 'source-over';
+    freeDrawCtx?.beginPath();
+    const image = freeDrawImageRef.current;
+    const layer = freeDrawLayerRef.current;
+
+    var localPos = {
+      x: lastPointerPosition!.x - image!.x(),
+      y: lastPointerPosition!.y - image!.y()
+    };
+    freeDrawCtx!.moveTo(localPos.x, localPos.y);
+    var pos = stage.getPointerPosition();
+    localPos = {
+      x: pos!.x - image!.x(),
+      y: pos!.y - image!.y()
+    };
+    freeDrawCtx!.lineTo(localPos.x, localPos.y);
+    freeDrawCtx!.closePath();
+    freeDrawCtx!.stroke();
+
+    lastPointerPosition = pos;
+    layer!.batchDraw();
+  }
 
   // TODO consider useLayoutEffect
   useEffect(() => {
@@ -21,8 +75,12 @@ function App() {
           width: containerRef.current.offsetWidth,
           height: containerRef.current.offsetHeight,
         });
+        freeDrawCanvas.width = width;
+        freeDrawCanvas.height = height;
       } else {
         setSize(defaultSize);
+        freeDrawCanvas.width = defaultSize.width;
+        freeDrawCanvas.height = defaultSize.height;
       }
     }
 
@@ -30,16 +88,26 @@ function App() {
 
     window.addEventListener("resize", resizeListener);
     return () => window.removeEventListener("resize", resizeListener);
-  }, []);
+  }, [containerRef]);
 
   return (
     <div className="App">
       <header className="App-header">Tarkov Debrief</header>
       <div className="Canvas" ref={containerRef}>
-        <Stage width={width} height={height}>
-          <Layer>
-            <Image image={mapImage} x={20} y={20} />
+        <Stage
+            width={width}
+            height={height}
+            onMouseDown={mouseDownHandler}
+            onTouchDown={mouseDownHandler}
+            onMouseUp={mouseUpHandler}
+            onTouchUp={mouseUpHandler}
+            onMouseMove={mouseMoveHandler}
+            onTouchMove={mouseMoveHandler}
+          >
+          <Layer ref={freeDrawLayerRef}>
+            {/*<Image image={mapImage} x={20} y={20} width={1000} height={500} />*/}
             <Rect x={20} y={20} width={250} height={250} fill={"green"} />
+            <Image image={freeDrawCanvas} ref={freeDrawImageRef} />
           </Layer>
         </Stage>
       </div>
