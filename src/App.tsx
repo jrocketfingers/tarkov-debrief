0da98fc884309ccc392cc895eb3c975cbe1b986c
Johnny Rocketfingers <developer94@gmail.com>
2021-01-21 04:07:53 +0100

    Toolbar, zoom, pen width based on zoom, bunch of useful shit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { fabric } from "fabric";
-import woodsMapUrl from "./woods.png";
+import woodsMapUrl from "./interchange.png";
 import "./App.css";
 import selectIcon from "./icons/select.svg";
 import pencilIcon from "./icons/pencil.svg";
@@ -12,11 +12,9 @@ type Size = { width: number; height: number };
 
 const defaultSize: Size = { width: 300, height: 300 };
 
-const freeDrawCanvas = document.createElement('canvas');
-const freeDrawCtx = freeDrawCanvas.getContext('2d');
-freeDrawCtx!.strokeStyle = '#df4b26';
-freeDrawCtx!.lineJoin = 'round';
-freeDrawCtx!.lineWidth = 5;
+type Tool = { active: boolean, type: 'select' | 'pencil' | 'eraser' };
+
+const tool: Tool = { type: 'pencil', active: true };
 
 function startDownload(url: string, name: string) : void {
   const link = document.createElement('a');
@@ -27,15 +25,19 @@ function startDownload(url: string, name: string) : void {
   document.body.removeChild(link);
 }
 
+const brushWidth = 5;
+
 function initializeCanvas() { 
   const canvas = new fabric.Canvas('canvas', {
     height: defaultSize.height,
     width: defaultSize.width,
-    isDrawingMode: true,
+    //isDrawingMode: true,
+    perPixelTargetFind: true,
+    selection: false,
   });
 
   canvas.freeDrawingBrush.color = 'red';
-  canvas.freeDrawingBrush.width = 5;
+  canvas.freeDrawingBrush.width = brushWidth;
 
   return canvas;
 }
@@ -53,14 +55,25 @@ function App() {
   }
 
   const select = () => {
+    tool.type = 'select';
     if (canvas) {
       canvas.isDrawingMode = false;
+      canvas.selection = true;
     }
   }
 
   const pencil = () => {
+    tool.type = 'pencil';
     if (canvas) {
       canvas.isDrawingMode = true;
+    }
+  }
+
+  const eraser = () => {
+    tool.type = 'eraser';
+    if (canvas) {
+      canvas.isDrawingMode = false;
+      canvas.selection = false;
     }
   }
 
@@ -74,6 +87,35 @@ function App() {
         canvas!.backgroundImage = image;
         setBackgroundImage(image);
       });
+
+      canvas.on('mouse:down', (opt) => {
+        tool.active = true;
+      });
+
+      canvas.on('mouse:up', (opt) => {
+        tool.active = false;
+      });
+
+      canvas.on('mouse:move', (opt) => {
+        if (opt.target === null) return;
+        if (tool.type === 'eraser' && tool.active) {
+          canvas.remove(opt.target!);
+        }
+      });
+
+      canvas.on('mouse:wheel', function(opt) {
+        const event = opt.e as WheelEvent;
+        const delta = event.deltaY;
+        let zoom = canvas.getZoom();
+        zoom *= 0.999 ** delta;
+        if (zoom > 20) zoom = 20;
+        if (zoom < 0.01) zoom = 0.01;
+        canvas.zoomToPoint({ x: event.offsetX, y: event.offsetY } as fabric.Point, zoom);
+        canvas.freeDrawingBrush.width = brushWidth / zoom;
+        opt.e.preventDefault();
+        opt.e.stopPropagation();
+      })
+
     }
 
     function resizeListener() {
@@ -93,7 +135,9 @@ function App() {
     resizeListener();
 
     window.addEventListener("resize", resizeListener);
-    return () => window.removeEventListener("resize", resizeListener);
+    return () => {
+      window.removeEventListener("resize", resizeListener);
+    }
   }, [containerRef, backgroundImage, canvas]);
 
   return (
@@ -103,7 +147,7 @@ function App() {
         <section className="App-header-buttons">
           <button onClick={select}><img src={selectIcon} alt="select" /></button>
           <button onClick={pencil}><img src={pencilIcon} alt="pencil" /></button>
-          <button><img src={undoIcon} alt="undo" /></button>
+          <button onClick={eraser}><img src={undoIcon} alt="undo" /></button>
           <button><img src={zoomIcon} alt="zoom" /></button>
           <button onClick={save}><img className="App-header-buttons-save" src={saveIcon} alt="save" /></button>
         </section>
